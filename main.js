13566b50ccd67e924364dd53651fe4b8291d914c
Bogdan-Lishchynskiy <lishchynskiy91@gmail.com>
2018-04-17 19:53:38 +0300

    fixed width container

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function (e) {
   console.log(document.body.scrollTop);
   var $parallax = document.getElementById('parallax');
-  var posY = window.pageYOffset * 1.6;
+  var posY = window.pageYOffset * 1.5;
   $parallax.style.backgroundPosition = '0px ' + (-posY) + 'px';
 })
